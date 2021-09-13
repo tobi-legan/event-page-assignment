b70bcd81f51a6cb92f13726d04570f9954255be9
tobi-legan <tobiadelegan70@gmail.com>
2021-09-13 06:24:11 +0100

    Extra tests under the search and filtering module

--- cypress/page-objects/submissions-page.js
+++ cypress/page-objects/submissions-page.js
@@ -1,3 +1,7 @@
+import {recurse} from 'cypress-recurse'
+import * as SubmissionsFiltersApiCalls from "../api-intercepts/filter-submissions-api";
+
+
 export function navigate() {
     cy.visit('event/5e733c5acde2b641284a7e27');
 }
@@ -26,7 +30,11 @@ export function closeAddFiltersSideMenu(){
 }
 
 export function clearAllFilters(){
+    cy.clickATextValue('Clear all filters');
+}
 
+export function searchUsingTheSearchField(placeholderValue,searchTerm){
+    cy.get('form').eq(1).findByPlaceholderText(placeholderValue).type(`${searchTerm}{enter}`, {force: true});
 }
 
 export function findElementWithPlaceholder(placeholderValue){
@@ -38,12 +46,90 @@ export function getImage(imageRoleName){
     return cy.findByRole('img', { name: `${imageRoleName}` });
 }
 
-export function getFilterAllFiltersForFilterGroup(filterGroupName){
+export function checkIfFilterGroupValuesAreDisplayed(filterGroupName, expectedNumberOfFilterValuesPlusTheShowMoreButton){
+
+    //The result of this is all the filter values under the filter group and the show 10 more button
+    cy.findAllByRole('heading', {name: filterGroupName}).parent().parent().siblings().within(() => {
+                cy.findAllByRole('button', {role: "button"}).should('have.length', expectedNumberOfFilterValuesPlusTheShowMoreButton);
+    });
+}
+
+export function checkEachFilterValueIsCorrectAfterUserClicksTheShowMoreButton(filterGroupName, filterValue, i, size){
+    cy.findAllByRole('heading', {name: filterGroupName}).parent().parent().siblings().within(() => {
+                cy.findAllByRole('button', {role: "button"}).should('have.length', size).eq(i).should('contain.text', filterValue);
+    });
+}
+
+export function compareFilterValueResultsFromTheApiWithTheResultsOnTheUi(filterGroupName, i, filterName){
+    cy.findAllByRole('heading', {name: filterGroupName}).parent().parent().siblings().within(() => {
+        cy.findAllByRole('button', {role: "button"}).eq(i).should('contain.text', filterName);
+    });
+}
+
+export function clickOnFilterCheckboxWithFilterText(filterGroupName, filterName){
 
     cy.findAllByRole('heading', {name: filterGroupName}).parent().parent().siblings().within(() => {
-                cy.findAllByRole('button', {role: "button"}).should('have.length', 11);
-            })
+                cy.clickATextValue(filterName);
+    });
+}
+
+export function clickOnTheShowMoreFilterButtonForAFilterGroup(howManyToBeDisplayed){
+
+    //getNumberFromFacetApi
+    //getPresentDisplayedValueNumber
+    //get
+    cy.findAllByRole('heading', {name: filterGroupName}).parent().parent().siblings().within(() => {
+        cy.clickATextValue(`Show ${howManyToBeDisplayed} more`);
+    });
+}
+
+export function checkIfFilterValueIsChecked(filterGroupName, filterName){
+    cy.findAllByRole('heading', {name: filterGroupName}).parent().parent().siblings().within(() => {
+        cy.findByText(filterName).siblings().first().within(() => {
+            cy.get('[type="checkbox"]').should('be.checked');
+        });
+    });
 }
 
 
-//for checkbox findByRole('checkbox', { checked: true })
+/* export function traverseFromPageOneToTheLastPage(){
+    var pages = 1;
+        recurse(
+            () => cy.findButton('Next'),
+            ($next) => !($next.is(":hidden")),
+            {  
+            
+                limit: 50, // max number of iterations
+                timeout: 30000,
+                
+                post(){
+
+                        SubmissionsFiltersApiCalls.interceptQuerySearchApiCall("rent", "5e733c5acde2b641284a7e27", (24*pages),24);
+                        cy.wait(3000);
+                        cy.findButton('Next').click(); 
+                        SubmissionsFiltersApiCalls.waitForQuerySearchApi("rent");
+
+                        pages++;
+                        //cy.findButton('Next').should('not.exist')
+                }
+            }
+        );
+    
+    
+}
+
+
+export function traverseFromLastPageToPageOne(){
+    recurse(
+        () => cy.findButton('Previous'),
+        ($next) => $next.should('not.exist'),
+        {    
+
+            post(){
+                cy.findButton('Previous').click();  
+            }
+
+        }
+        
+    );
+} */
